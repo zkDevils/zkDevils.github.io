ae477f3d2fc99e72ffd47c123a243274f891c47f
zkDevils <138419043+zkDevils@users.noreply.github.com>
2023-07-04 16:21:48 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,13 +4,14 @@ document.addEventListener('DOMContentLoaded', function() {
     const landingPage = document.getElementById('landing-page');
     const overlayHeight = overlay.offsetHeight;
     const landingPageHeight = landingPage.offsetHeight;
-    const scrollThreshold = 50; // Adjust this value to control the scroll threshold for the animation
-    const overlaySpeed = 2.5; // Adjust this value to control the scrolling speed
+    const scrollThreshold = 300; // Adjust this value to control the scroll threshold for the animation
+    const overlaySpeed = 0.5; // Adjust this value to control the scrolling speed
 
     function handleScroll() {
         const currentScroll = window.pageYOffset;
         const overlayOffset = Math.min(currentScroll * overlaySpeed, scrollThreshold);
         const contentOpacity = 1 - (currentScroll / scrollThreshold);
+    
 
         overlay.style.top = `calc(50% + ${overlayOffset}px)`;
         content.style.opacity = contentOpacity > 0 ? contentOpacity : 0;
