3114db28e0968330d7f8e5f34fc9c45f3bcd66bb
zkDevils <138419043+zkDevils@users.noreply.github.com>
2023-07-03 22:40:39 -0400

    add on script

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
     const overlay = document.getElementById('overlay');
     const landingPage = document.getElementById('landing-page');
+    const overlayHeight = overlay.offsetHeight;
+    const landingPageHeight = landingPage.offsetHeight;
     let previousScroll = 0;
 
     function handleScroll() {
         const currentScroll = window.pageYOffset;
         const scrollDelta = currentScroll - previousScroll;
+        const opacity = 1 - (currentScroll / (landingPageHeight - overlayHeight));
 
-        if (scrollDelta > 0) {
-            overlay.style.opacity = 1 - (currentScroll / (document.documentElement.scrollHeight - window.innerHeight));
-        }
+        overlay.style.opacity = opacity > 0 ? opacity : 0;
 
         previousScroll = currentScroll;
     }
